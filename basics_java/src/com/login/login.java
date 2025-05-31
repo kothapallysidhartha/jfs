b3d7023d61299d0ddba4f1f130d88789d87afe7d
Kothapelly Sai Sidhartha <kothapallysidhartha@gmail.com>
2025-05-31 17:17:26 +0530

    🔥 Initial commit - pushed all files

---
+++ basics_java/src/com/login/login.java
@@ -0,0 +1,11 @@
+package com.login;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("login from com.login");
+
+	}
+
+}
