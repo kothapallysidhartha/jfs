655bbdbe2673abfe84a9a9394c9d38f5986175f1
Kothapelly Sai Sidhartha <kothapallysidhartha@gmail.com>
2025-06-01 22:59:46 +0530

    JS

---
+++ JavaScript/first.js
@@ -0,0 +1,16 @@
+console.log('This is the first JS.');
+var age= 25;
+let price=1200;
+let person = "John Doe";
+const greeting = "Hello, World!";
+console.log(greeting);
+console.log(`Name: ${person}, Age: ${age}, Price: $${price}`);
+const student = {
+  name: "Alice",
+  age: 22,
+  major: "Computer Science"
+};
+console.log(typeof student); // "object"
+console.log(student.major)
+student.age = 23; // Update age
+console.log(`Updated Age: ${student.age}`);
